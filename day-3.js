a3970befafbd0778eb0529c7dd4bc4ca881afb96
christinamartinez <christina.martinez@faithlife.com>
2022-12-16 17:15:22 -0800

    day 2, part 2.

--- day-3.js
+++ day-3.js
@@ -4,81 +4,86 @@ const pre = document.getElementsByTagName("pre");
 const allRucksacks = pre[0].innerHTML;
 
 function checkIfRucksackIncludesItem(
-    firstCompartment,
-    secondCompartment,
+    firstString = "",
+    secondString = "",
     letters
 ) {
-    // I thought about creating a set rather than using string.includes, because searching a set is O(1). However, since we start with a string, we'd need to iterate through it once to create the set, which makes it O(n) for creation plus O(1) for the search, which would be practically bigger and approximately the same. For this reason I will go with string.includes. If I wanted to go through the compartment more than once, I would use a set instead.
-    for (let i = 0; i < secondCompartment.length; i++) {
-        if (firstCompartment.includes(secondCompartment[i])) {
-            letters.push(secondCompartment[i]);
-            return;
+    // I thought about creating a set but thought that it wouldn't make a big difference in speed (I was wrong, I discovered after a convo with Jacob.). I guess I should refactor to use map...
+    for (let i = 0; i < secondString.length; i++) {
+        if (firstString.includes(secondString[i])) {
+            letters.push(secondString[i]);
         }
     }
-    console.log("no match found");
-    return null;
+    return;
 }
 
 function getPriorityPointsForLetter(letter) {
+    if (!letter) return 0;
     let points = null;
     let letterCopy = letter;
 
     // A = ascii code 65 (since the points start at 27, we use 65 - 27 = )
     // a = ascii code 97 (since the points start at 1, we use 96)
     if (letter === letterCopy.toUpperCase()) {
-        points = letter.charCodeAt(letter[0]) - 38;
+        points = letter.charCodeAt(0) - 38;
     } else if (letter === letterCopy.toLowerCase()) {
-        points = letter.charCodeAt(letter[0]) - 96;
+        points = letter.charCodeAt(0) - 96;
     }
     return points;
 }
 
-function sumPriorities(letters) {
-    let priorities = 0;
-
-    for (letter of letters) {
-        priorities += getPriorityPointsForLetter(letter);
-    }
-    return priorities;
-}
-
-function checkRucksacksAndSumPoints(allRucksacks) {
-    const rucksacksArray = allRucksacks.split("\n");
+function compareThreeRucksacks(arrayOfRucksacks) {
     const letters = [];
-    for (rucksack of rucksacksArray) {
-        // I could have modified the original array using splice for more concision but I'm chosing readability.
-        let firstCompartment = rucksack.slice(0, rucksack.length / 2);
-        const secondCompartment = rucksack.slice(
-            rucksack.length / 2,
-            rucksack.length
-        );
-        checkIfRucksackIncludesItem(
-            firstCompartment,
-            secondCompartment,
-            letters
-        );
+    // if there's only one common letter, we can probably assume that that's the one, but I guess we should check for good measure
+    checkIfRucksackIncludesItem(
+        arrayOfRucksacks[0],
+        arrayOfRucksacks[1],
+        letters
+    );
+
+    let letterString = "";
+    for (let j = 0; j < letters.length; j++) {
+        letterString = letterString.concat(letters[j]);
     }
-    return sumPriorities(letters);
+    const lettersInAllThree = [];
+    checkIfRucksackIncludesItem(
+        arrayOfRucksacks[2],
+        letterString,
+        lettersInAllThree
+    );
+    // we can assume that there is only one. If this was real life, I would have validated this, stopped when we found the first letter that matches, etc.
+    return lettersInAllThree[0];
 }
 
-// time complexity: O(n)
-// we have two processes: for the first, we go through each line and iterate through half of each line, checking the second half against the first. This would be a time complexity of O(n/2), but string.includes is linear, so it's actually O(n). Regardless, using string.includes doesn't impact the approximate time complexity since O(n/2) is around the same as O(n).
-// for the second, we go through each letter (one per line) and check it, but since this is faster than the first process, we can ignore it and say that the entire process is O(n)
+function findBadgeType(allRucksacks, points, indexStart) {
+    let currentRucksacks = [];
+    while (indexStart < allRucksacks.length) {
+        currentRucksacks = [];
+        for (let i = indexStart; i < indexStart + 3; i++) {
+            currentRucksacks.push(allRucksacks[i]);
+        }
+        points += getPriorityPointsForLetter(
+            compareThreeRucksacks(currentRucksacks)
+        );
+        indexStart += 3;
+    }
+    return points;
+}
 
-// space complexity: O(2n) = O(n)
-// I made a copy of each line, split into two strings to represent the compartments, so I'm doubling the space used when I did that. That was a concious choice for readability, but if space mattered, I wouldn't have done that.
-// I also created a letters array, but that is smaller than the compartments values so the size doesn't matter much.
+function partTwo(allRucksacks) {
+    const rucksacksArray = allRucksacks.split("\n");
+    rucksacksArray.pop(); // because I'm running it in the browser, it gives me an extra blank line that I don't want to deal with
+    points = findBadgeType(rucksacksArray, 0, 0);
+    return points;
+}
+
+// time complexity: O(n^2)?
+// I go through each line (in groups of 3), comparing the first line to the second using string.includes (linear search). Then I compare the commonalities between the two to the third string (another linear search). I think that's O(n^2).
+// space complexity: O(n)
 
 const testInput = `vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw`;
 
-console.group("TESTS");
-console.log(
-    `the test input returns the correct number: ${
-        checkRucksacksAndSumPoints(testInput) === 157
-    }`
-);
-console.log(`part 1: ${checkRucksacksAndSumPoints(allRucksacks)}`);
-console.groupEnd();
+console.log(`part 2: ${partTwo(allRucksacks)}`);
 
 /*
  some stuff I learned more about:
