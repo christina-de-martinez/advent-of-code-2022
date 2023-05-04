f91059567678ef551cac3fe946eb6dcd333d121a
christinamartinez <christina.martinez@faithlife.com>
2023-05-04 16:58:09 -0700

    more work on day 8 part 1... it works with the example set but not with the real data set.

--- day-8.js
+++ day-8.js
@@ -1,6 +1,6 @@
 const pre = document.getElementsByTagName("pre");
 const realInput = pre[0].innerHTML;
-const exampleInput = `30373\n25512\n65332\n33549\n35390`;
+// const exampleInput = `30373\n25512\n65332\n33549\n35390`;
 
 function makeMatrix(rows) {
     let matrix = [];
@@ -35,64 +35,43 @@ function gatherTrees(
     return row;
 }
 
-function checkRow(matrix, position, direction) {
-    let startingValue = null;
-    let endingValue = null;
-    let whichPosToReplace = 0;
-    let isVisible = false;
+function checkRow(row, reverse = false) {
+    if (row.length <= 0) return;
+    // for each row, reduce and keep track of the highest number
+    // when you encounter a new highest number, add 1 to visible trees and keep the new highest number as the new standard
+    let tallestTreeSoFar = parseInt(reverse ? row[row.length - 1] : row[0]);
+    // const visibleTreeLocations = [reverse ? row.length - 1 : 0];
+    const visibleTreeLocations = [];
 
-    if (direction === "left") {
-        startingValue = 0;
-        endingValue = position[1];
-    } else if (direction === "right") {
-        startingValue = position[1] + 1;
-        endingValue = matrix[position[0]].length;
-    } else if (direction === "up") {
-        startingValue = 0;
-        endingValue = position[0];
-        whichPosToReplace = 1;
-    } else if (direction === "down") {
-        startingValue = position[1] + 1;
-        endingValue = matrix[position[1]].length;
-        whichPosToReplace = 1;
-    }
-    const rowToCheck = gatherTrees(
-        matrix,
-        position,
-        startingValue,
-        endingValue,
-        whichPosToReplace
-    );
-
-    if (matrix[position[0]][position[1]] > findTallestTree(rowToCheck)) {
-        isVisible = true;
-        console.log(
-            rowToCheck,
-            `${
-                matrix[position[0]][position[1]]
-            } is taller than ${findTallestTree(
-                rowToCheck
-            )}, so its visible from ${direction}`
-        );
-    }
-    return isVisible;
+    return row.reduce((acc, curr, idx) => {
+        const currentTreeHeight = parseInt(curr);
+        if (currentTreeHeight > tallestTreeSoFar) {
+            tallestTreeSoFar = currentTreeHeight;
+            visibleTreeLocations.push(reverse ? row.length - idx - 1 : idx);
+        }
+        return visibleTreeLocations;
+    });
 }
 
-function checkTree(matrix, position) {
-    let isVisible = false;
+function scan(row) {
+    // forward
+    const visibleTreeLocationsForward = checkRow(row);
 
-    isVisible = checkRow(matrix, position, "left");
-    if (!isVisible) {
-        isVisible = checkRow(matrix, position, "right");
+    // backward
+    const reversedRow = row.slice().reverse();
+    const visibleTreeLocationsReverse = checkRow(reversedRow, true);
+
+    if (!visibleTreeLocationsForward || !visibleTreeLocationsReverse) {
+        return;
     }
-    if (!isVisible) {
-        isVisible = checkRow(matrix, position, "up");
-    }
-    if (!isVisible) {
-        isVisible = checkRow(matrix, position, "down");
-    }
-    console.log("----checking", position);
-    return isVisible;
+    const allVisibleTreeLocations = [
+        ...visibleTreeLocationsForward,
+        ...visibleTreeLocationsReverse,
+    ];
+    // remove duplicates using a set
+    if (!allVisibleTreeLocations) return;
+    const visibleTrees = [...new Set(allVisibleTreeLocations)];
+    return visibleTrees;
 }
 
 function part1(input) {
@@ -109,25 +88,36 @@ function part1(input) {
     // dont double-count corners
     const allEdges = horizontalEdges + verticalEdges - 4;
     visibleTrees += allEdges;
-    // console.log("visible trees from edges", visibleTrees);
 
-    for (let j = 1; j < matrix.length - 1; j++) {
-        for (let i = 1; i < matrix[1].length - 1; i++) {
-            if (checkTree(matrix, [j, i])) {
-                visibleTrees++;
-            }
-        }
+    // scan horizontally
+    // keep track of locations of visible trees as they're found
+    const visibleTreeLocations = {};
+    for (let i = 1; i < matrix.length - 1; i++) {
+        visibleTreeLocations[i] = new Set(scan(matrix[i]));
     }
+
+    // build columns and scan vertically
+    for (let k = 1; k < matrix[0].length - 1; k++) {
+        const column = [];
+        for (let j = 0; j < matrix.length; j++) {
+            column.push(matrix[j][k]);
+        }
+        const verticalPositions = scan(column);
+        if (!verticalPositions) return;
+        visibleTreeLocations[k].add(...verticalPositions);
+    }
+    console.log(visibleTreeLocations);
+
+    // remove first and last trees since we already accounted for them (the whole point of that was to avoid doing extra work but now we have to do extra work. lol.)
+    for (const [key, value] of Object.entries(visibleTreeLocations)) {
+        value.delete(0);
+        value.delete(matrix[0].length - 1);
+        visibleTrees += value.size;
+    }
+
     return visibleTrees;
-    // check each tree in matrix (other than the edges) - if checkTree returns true it's visible so you can add it to the count.
-
-    /*
-    go from matrix[1][1] to matrix[1][length - 2]
-    matrix[2][1] to matrix[2][length - 2] etc.
-
-    and matrix[length -2][1] to matrix[length - 2][length - 2]
-    */
 }
 
 const visibleTrees = part1(realInput);
+// const visibleTrees = part1(exampleInput);
 console.log(visibleTrees);
